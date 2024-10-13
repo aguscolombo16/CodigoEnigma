69a53eb8dc3a651f349a2cf018c168bca145a3d1
Agustin Colombo <aguscolombo16@gmail.com>
2024-10-13 12:31:39 -0300

    ingresamos la clase 4 parte 1 y 2 y clase 5 parte 1 y 2 a Git Hub

---
+++ TecnicaturaGit/Python/Clase_5_parte2/Ejercicio_3_conFunciones.py
@@ -0,0 +1,14 @@
+#Ejercicio N° 3: Funcion Recursiva 
+#Imprimir numeros de 5 a 1 de manera descendente usando fundiones recursivas 
+#Puede ser cualquier valor positivo , por ejemplo , si pásamos el
+#valor de 5 , debe imprimir:
+#5
+#4
+#3
+#2
+#1
+#En caso de ser el numero 3 debe imprimir 
+#3
+#2
+#1
+#Si se ingresan numeros negativos no imprime nada
